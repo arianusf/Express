46ed92d6a504bbb4a2b258fe52022a3977d25eeb
Arian yousefzadeh <arian.yousefzadeh@star-cooperation.com>
2024-05-03 14:31:45 +0200

    .shopping

--- index.js
+++ index.js
@@ -1,32 +1,65 @@
-const express = require('express'); 
-const fs = require('node:fs');
-const app = express(); 
-const port = 5000; 
+const express = require('express');
+const { JsonDB, Config } = require('node-json-db');
+const app = express();
+app.use(express.json());
+const port = 5000;
+
+var db = new JsonDB(new Config('data', true, false, '/'));
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html', { root: __dirname });
+    res.sendFile('index.html', { root: __dirname });
 });
 
 app.get('/json', (req, res) => {
-  res.sendFile('data.json', { root: __dirname });
+    res.sendFile('data.json', { root: __dirname });
 });
 
-app.get('/setJson', (req, res) => {
-
-  const content = `{
-     "name":${Math.random()*100}
-}`;
-
-  fs.writeFile('./data.json', content, (err) => {
-    if (err) {
-        console.log(err)
-        res.send('no');
-    } else {
-        res.send('yes');
+app.get('/shopping', async (req, res) => {
+    try {
+        var data = await db.getData('/shopping');
+        res.send({ values: data });
+    } catch (e) {
+        res.send({ error: e });
     }
-  });
+});
+
+app.post('/shopping', async (req, res) => {
+    const item = req?.body?.name;
+    if (item) {
+        var data = await db.getData('/shopping');
+        if (!data.find((x) => x === item)) {
+            data.push(item);
+        }
+        await db.push('/shopping', data);
+        res.send({ values: data });
+    } else {
+        res.send({ error: "can't insert item" });
+    }
+});
+
+app.get('/todo', async (req, res) => {
+    try {
+        var data = await db.getData('/todo');
+        res.send({ values: data });
+    } catch (e) {
+        res.send({ error: e });
+    }
+});
+
+app.post('/todo', async (req, res) => {
+    const item = req?.body?.name;
+    if (item) {
+        var data = await db.getData('/todo');
+        if (!data.find((x) => x === item)) {
+            data.push(item);
+        }
+        await db.push('/todo', data);
+        res.send({ values: data });
+    } else {
+        res.send({ error: "can't insert item" });
+    }
 });
 
 app.listen(port, () => {
-  console.log(`Now listening on port ${port}`);
+    console.log(`Now listening on port ${port}`);
 });
