0943086dada27c7c2b5dff13d267a8d10c775615
Arian yousefzadeh <arian.yousefzadeh@star-cooperation.com>
2024-05-03 11:07:43 +0200

    third

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-const express = require('express'); //Import the express dependency
-const app = express();              //Instantiate an express app, the main work horse of this server
-const port = 5000;                  //Save the port number where your server will be listening
+const express = require('express'); 
+const fs = require('node:fs');
+const app = express(); 
+const port = 5000; 
 
-//Idiomatic expression in express to route and respond to a client request
-app.get('/', (req, res) => {        //get requests to the root ("/") will route here
-    res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser                                                //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: __dirname });
 });
 
-app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
-    console.log(`Now listening on port ${port}`); 
+app.get('/json', (req, res) => {
+  res.sendFile('data.json', { root: __dirname });
+});
+
+app.get('/setJson', (req, res) => {
+
+  const content = `{
+     "name":${Math.random()*100}
+}`;
+
+  fs.writeFile('./data.json', content, (err) => {
+    if (err) {
+        console.log(err)
+        res.send('no');
+    } else {
+        res.send('yes');
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Now listening on port ${port}`);
 });
